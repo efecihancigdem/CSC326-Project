c5eb3afeb2ab4373685171498c6d4171232a9252
mrtoss <43189664+mrtoss@users.noreply.github.com>
2018-09-27 12:53:06 -0400

    Added a website that displays a htm file

--- sampleBottleFramework/venv/sample.py
+++ sampleBottleFramework/venv/sample.py
@@ -1,7 +1,14 @@
-from bottle import route, run
+from bottle import route, run, static_file
 
 @route('/hello')
 def hello():
     return "<h1>Hello world!</h1>"
+
+#this is a website that displays an htm file
+@route('/fromFile/<fileName>')
+def fromFile(fileName):
+    #to eliminate the need for users to type in the file type
+    fileName=fileName+'.htm'
+    return static_file(fileName, root= 'websites')
 
 run(host='localhost', port=8080, debug=True)
