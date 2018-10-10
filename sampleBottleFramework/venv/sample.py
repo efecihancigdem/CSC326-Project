992fce91c84b29f14b77fadb3ea66911df0c0178
efecihancigdem <cefecigdem@gmail.com>
2018-10-09 20:18:00 -0400

    commented sample

--- sampleBottleFramework/venv/sample.py
+++ sampleBottleFramework/venv/sample.py
@@ -1,70 +1,31 @@
 from bottle import route, run, static_file, request, template
 from collections import Counter
 
+#Global counter to keep track of search history
 search_history = Counter()
-listed= ['brandon','efe']
-freq = [2,1]
 
-@route('/hello')
-def hello():
-    return "<h1>Hello world!</h1>"
-
-#this is a website that displays an htm file
+#The fucntion that handles main page and result page
 @route('/')
-# def fromFile():
-#     #to eliminate the need for users to type in the file type
-#     fileName='home'
-#     words = history_word_parse()
-#     count = history_count_parse()
-#     return template(fileName,name=words,freq=count)
-
 def display_search():
-    query = request.query.search
-    if query=="":
+    query = request.query.search #getting url query
+    if query == "": #Checking if it is home page request or search request
         fileName = 'home'
         words = history_word_parse()
         count = history_count_parse()
         return template(fileName,name=words,freq=count)
-    words_in_quesry = query.split(" ")
-    my_url = "query="
-    for word in words_in_quesry:
-        my_url += word + "+"
+    # Parsing the search dividing the word and the frequency into 2 lists as words and count
     parsed_dict = word_count(query)
     words = search_word_parse(parsed_dict)
     count = search_count_parse(parsed_dict)
-    # redirect("/search/<my_url>")
+    return template('search', name=words, freq=count, query=query) # sending lists to template to display
 
-    return template('search', name=words, freq=count, query=query)
+#Function to reach our logo
 @route('/image/<picture>')
 def serve_pictures(picture):
     picture = picture+'.png'
     return static_file(picture, root='websites')
 
-#this is the result of login page
-@route('/login')
-def login():
-    return static_file('login.htm', root= 'websites')
-
-# @route('/', method='POST')
-# def parse():
-#     query = request.form.get('search')
-#     words_in_quesry=query.split(" ")
-#     my_url = "query="
-#     for word in words_in_quesry:
-#         my_url += word + "+"
-#     parsed_dict = word_count(query)
-#     words = search_word_parse(parsed_dict)
-#     count = search_count_parse(parsed_dict)
-#     #redirect("/search/<my_url>")
-#     return template('search', name = words,freq = count,query = query)
-
-
-
-#For users with the link
-@route('/search/query=<query>')
-def user_with_link(query):
-    return '<p>You are looking for a search result: '+query +'.</p>'
-
+#Function to increment search history and count occurance of the keywords
 def word_count(str):
     counts = dict()
     words = str.split()
@@ -76,6 +37,7 @@ def word_count(str):
             counts[word] = 1
     return counts
 
+#Making a list of keywords from the most common 20 keywords in search_history variable
 def history_word_parse():
     words = []
     i=0
@@ -84,6 +46,7 @@ def history_word_parse():
         i+=1
     return words
 
+#Making a list of number of occurances corresponding to most common 20 keywords from search_history variable
 def history_count_parse():
     freq = []
     i = 0
@@ -91,6 +54,8 @@ def history_count_parse():
         freq.append(count)
         i += 1
     return freq
+
+#Making a list of keywords from the search query
 def search_word_parse(results):
     words = []
     i=0
@@ -99,6 +64,7 @@ def search_word_parse(results):
         i+=1
     return words
 
+#Making a list of occurances from the search query
 def search_count_parse(results):
     freq = []
     i = 0
@@ -106,8 +72,6 @@ def search_count_parse(results):
         freq.append(results[word])
         i += 1
     return freq
-run(port=8090)
+run(port=8080)
 
-#Template for table results page#
-#Request used for getting quesries in the Url and can be used
-#in the def of the route and param1=request.quesry.param1
+
