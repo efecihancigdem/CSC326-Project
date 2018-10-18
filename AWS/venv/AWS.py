a61683912dc7764105a0c20b1cacb5bc1697860f
mrtoss <43189664+mrtoss@users.noreply.github.com>
2018-10-17 23:03:51 -0400

    deleted KeyPair for security reasons

--- AWS/venv/AWS.py
+++ AWS/venv/AWS.py
@@ -1,6 +1,12 @@
 import boto.ec2
 
+#A security group is already created using the protocols provided, its name is "csc326lab2"
+#a key pair is also created called "ourKeyPair"
+#an instance has been run with amiID "ami-9aaa1cf2"
+
+#set up the region we will use
 conn = boto.ec2.connect_to_region("us-east-1")
-key_pair=conn.delete_key_pair("ourKeyPair")
-key_pair=conn.create_key_pair("ourKeyPair")
-key_pair.save("")
+group=conn.get_all_security_groups()
+resp=conn.get_all_instances()
+print "hello"
+
