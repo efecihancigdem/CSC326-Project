b4c99de998c2c1befbcabdb939db634b704e1632
mrtoss <43189664+mrtoss@users.noreply.github.com>
2018-10-27 13:53:48 -0400

    updated AWS.py for submission

--- AWS/venv/AWS.py
+++ AWS/venv/AWS.py
@@ -6,7 +6,9 @@ import boto.ec2
 
 #set up the region we will use
 conn = boto.ec2.connect_to_region("us-east-1")
-group=conn.get_all_security_groups()
-resp=conn.get_all_instances()
-print "hello"
+key_pair = conn.create_key_pair("ourKeyPair")
+group =  conn.create_security_group("csc326-group27")
+group.authorize("icmp", -1, -1, "0.0.0.0/0")
+group.autorize("tcp", 22, 22, "0.0.0.0/0")
+group.autorize("tcp", 80, 80, "0.0.0.0/0")
 
